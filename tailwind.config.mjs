91f5c7aa488a9009dc63de664810f689e731fb8e
Inzimam <hi@inhaq.me>
2024-01-02 22:19:27 +0500

    Added custom color palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: ['tailwind-plugin-prettier'],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        arctic: "#F1F6F4",
+        mint: "#D9E8E3",
+        forsythia: "#FFC801",
+        saffron: "#FF9932",
+        nocturnal: "#114C5A",
+        noir: "#172B36",
+        dead: "#191f28",
+      },
+    },
+  },
+  plugins: ["tailwind-plugin-prettier"],
+};
