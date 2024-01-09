b1c299f969c8caf3d4b0da7b52fb6d198f71b8ae
Inzimam <hi@inhaq.me>
2024-01-09 23:55:51 +0500

    hero partly

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -8,36 +8,26 @@ export default function Shimmer() {
         "--highlight": "255 255 255",
         "--bg-color":
           "linear-gradient(rgb(var(--background)), rgb(var(--background)))",
-        "--border-color": `linear-gradient(0deg,
-            rgba(255, 255, 255, 0) 0%,
-            rgba(255, 255, 255, 1) 50%,
-            rgba(255, 255, 255, 0) 100%)
-          `,
       }}
-      className="relative flex aspect-[2/1] w-full max-w-md flex-col items-center justify-center rounded-xl border border-transparent p-8 text-center
+      className="relative flex aspect-[2/1] w-full max-w-lg flex-col items-center justify-center rounded-xl border border-transparent p-8 text-center
       [background:padding-box_var(--bg-color),border-box_var(--border-color)] overflow-hidden"
     >
       <motion.div
-        initial={{ opacity: 1 }}
         animate={{
-          x: ["-100%", "100%"],
-          opacity: 0.5,
+          x: ["-270px", "250px"],
+          opacity: [0.5, 0.8, 0.6, 0.4, 0.2, 0.1, 0],
         }}
         transition={{
-          duration: 10,
+          duration: 3,
           repeat: Infinity,
-          ease: "linear",
-          opacity: "0.5",
+          ease: "easeOut",
         }}
-        className="absolute inset-y-0 w-full h-[1px]"
+        className="absolute inset-y-0 w-10 h-[3px]"
         style={{
-          background: `linear-gradient(296deg,
-            rgba(255, 255, 255, 0) 0%,
-            rgba(255, 255, 255, 1) 5%,
-            rgba(255, 255, 255, 0) 10%)`,
+          background: `linear-gradient(270deg,rgba(255,255,255,.6) 0%,hsla(0,0%,100%,0) 100%)`,
         }}
       />
-      <p className="text-xl font-medium text-white z-10 relative">
+      <p className="text-xl w-100 font-medium text-white z-10 relative">
         Hello, gradient
       </p>
     </div>
