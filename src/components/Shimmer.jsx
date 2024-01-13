f171e20110db977de0580e45a30aae260f8d0216
Inzimam <hi@inhaq.me>
2024-01-13 23:27:06 +0500

    add hero img, work, edu

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -14,26 +14,26 @@ export default function Shimmer() {
         "--bg-color":
           "linear-gradient(rgb(var(--background)), rgb(var(--background)))",
       }}
-      className="shimmer-container relative flex aspect-[2/1] w-full max-w-[1320px] flex-col items-center justify-center rounded-xl border border-transparent  text-center
-      [background:padding-box_var(--bg-color),border-box_var(--border-color)] overflow-hidden"
+      className="shimmer-container relative flex aspect-[2/1] w-full flex-col items-center justify-center rounded-xl border border-transparent  text-center
+      [background:padding-box_var(--bg-color),border-box_var(--border-color)] overflow-hidden p-20"
     >
       <motion.div
         animate={{
-          x: ["0px", "1320px"],
+          x: ["0px", "1300px"],
           opacity: [0.5, 0.8, 1, 0.7, 0.5, 0.3, 0],
         }}
         transition={{
-          duration: 4,
+          duration: 3,
           repeat: Infinity,
           ease: "easeOut",
         }}
-        className="shimmer-effect absolute inset-0 w-10 h-[3px]"
+        className="shimmer-effect absolute inset-0 w-10 h-[4px]"
         style={{
           background: `linear-gradient(270deg,rgba(255,255,255,.6) 0%,hsla(0,0%,100%,0) 100%)`,
         }}
       />
-      <div className="w-[100%] h-[100%] main-hero">
-        <motion.p className="z-10 relative p-14 font-title text-5xl text-mint text-left max-w-screen-lg">
+      <div className="w-[100%] h-[100%] flex z-10">
+        <motion.p className="z-10 relative font-title text-5xl text-mint text-left max-w-screen-lg mr-10">
           {text.split("").map((char, index) => (
             <motion.span
               key={index}
@@ -51,6 +51,13 @@ export default function Shimmer() {
             </motion.span>
           ))}
         </motion.p>
+        <div className="max-w-xs px-2.5 lg:max-w-none">
+          <img
+            className=" aspect-auto rotate-3 max-w-80 rounded-3xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            src="./../../public/profile.jpg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
